38a25ef83e08e15092a123a42e6bc85df3dc5911
MohanscodeHub <Krishnanmohan49@yahoo.com>
2022-05-07 23:04:15 +0530

    Added utility methods to PageActions.java

--- src/main/java/com/clipboard/utils/PageActions.java
+++ src/main/java/com/clipboard/utils/PageActions.java
@@ -1,13 +1,16 @@
-
 package com.clipboard.utils;
 
 
 import com.clipboard.driver.DriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
 
 
 public final class PageActions {
@@ -16,20 +19,19 @@ public final class PageActions {
 
     }
 
-    public static void waitAndClick(By by){
+    public static void waitAndClick(By by) {
         waitForElementToBePresent(by).click();
 
     }
 
 
-    public static String getTitle(){
+    public static String getTitle() {
         return DriverManager.getDriver().getTitle();
 
     }
 
-    public static WebElement waitForElementToBePresent(By by){
-        return new WebDriverWait(DriverManager.getDriver(),10)
-                .until(ExpectedConditions.presenceOfElementLocated(by));
+    public static WebElement waitForElementToBePresent(By by) {
+        return new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.presenceOfElementLocated(by));
 
     }
 
@@ -40,7 +42,33 @@ public final class PageActions {
         js.executeScript("window.scrollBy(0,50)", "");
     }
 
-    public static String getText(By by){
+    public static String getText(By by) {
         return waitForElementToBePresent(by).getText();
     }
+
+    public static void switchToNewlyOpenedWindow() {
+        String parentWinHandle = DriverManager.getDriver().getWindowHandle();
+
+        Set<String> winHandles = DriverManager.getDriver().getWindowHandles();
+        for (String temp : winHandles) {
+            if (!temp.equalsIgnoreCase(parentWinHandle)) {
+                DriverManager.getDriver().switchTo().window(temp);
+
+            }
+        }
+    }
+
+    public static String getHref() {
+        return DriverManager.getDriver().findElement(By.xpath("(//span[@class='a-price-whole'])[2]//parent::*//parent::*//parent::a")).getAttribute("href");
+
+    }
+
+    public static void openlinkinnewwindow(By by) {
+        Actions act = new Actions(DriverManager.getDriver());
+        act.moveToElement(DriverManager.getDriver().findElement(by)).contextClick().sendKeys(Keys.DOWN).sendKeys(Keys.DOWN).sendKeys(Keys.ENTER).build().perform();
+
+    }
+
 }
+
+
