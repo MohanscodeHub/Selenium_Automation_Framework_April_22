9503a47fca867aa342245ab02d0770beedd9a093
MohanscodeHub <Krishnanmohan49@yahoo.com>
2022-05-09 22:32:44 +0530

    updated  FrameworkConstants.java with extent reports.

--- src/main/java/com/clipboard/constants/FrameworkConstants.java
+++ src/main/java/com/clipboard/constants/FrameworkConstants.java
@@ -8,9 +8,28 @@ public class FrameworkConstants {
 
 
     private static final String CHROMEDRIVERPATH  = System.getProperty("user.dir")+"/src/test/resources/Executables/chromedriver.exe";
+    private static final String CONFIGFILEPATH  = System.getProperty("user.dir")+"/src/test/resources/config.properties";
+    private static final String EXTENTREPORTPATH  = System.getProperty("user.dir")+"/src/test/resources/config.properties";
 
+    private static final int TIMEOUT = 10;
     public static String getChromedriverpath()
     {
         return CHROMEDRIVERPATH;
     }
+
+    public static String getConfigfilepath()
+    {
+        return CONFIGFILEPATH;
+    }
+
+    public static int getTIMEOUT()
+    {
+        return TIMEOUT;
+    }
+
+    public static String getExtentreportpath()
+    {
+        return EXTENTREPORTPATH;
+    }
+
 }
