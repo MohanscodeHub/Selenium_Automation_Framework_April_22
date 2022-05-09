1e196e74864ad41e00d98187f35da01e86bdb339
MohanscodeHub <Krishnanmohan49@yahoo.com>
2022-05-09 16:33:37 +0530

    Added ExtentLogger.java.java for logging the pass,fail and skipped events.

---
+++ src/main/java/com/clipboard/reports/ExtentLogger.java
@@ -0,0 +1,29 @@
+package com.clipboard.reports;
+
+
+
+
+
+
+import static com.clipboard.reports.ExtentManager.getExtentTest;
+
+public final class ExtentLogger {
+
+    private ExtentLogger() {
+
+    }
+
+    public static void pass(String message){
+        getExtentTest().pass("message");
+    }
+
+   public static void fail(String message){
+      getExtentTest().fail("message");
+  }
+
+  public static void skip(String message){
+      getExtentTest().skip("message");
+   }
+
+
+}
