935c244a20d084e943ebf62f43cc2070c0516b90
MohanscodeHub <Krishnanmohan49@yahoo.com>
2022-05-11 09:05:39 +0530

    renamed the classfile

---
+++ src/main/java/com/clipboard/pages/ProductDetailsPage.java
@@ -0,0 +1,26 @@
+package com.clipboard.pages;
+
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import static com.clipboard.utils.Genericutils.getListOfItems;
+import static com.clipboard.utils.PageActions.*;
+
+public final class ProductDetailsPage {
+
+    private static final By productDescriptionHeader = By.xpath("//h1[text()=' About this item ']");
+    private static final By productFeaturesList = By.xpath("//h1[@class='a-size-base-plus a-text-bold']//following-sibling::ul/li");
+
+    public String getAboutThisItem(){
+
+        return getTextByElement(productDescriptionHeader);
+    }
+
+    public List<String> getProductDetails(){
+
+       return getListOfItems(productFeaturesList);
+
+    }
+
+}
---
+++ src/test/java/com/clipboard/tests/ProductDetailsTests.java
@@ -0,0 +1,33 @@
+package com.clipboard.tests;
+
+import com.clipboard.base.BaseTest;
+import com.clipboard.pages.ProductDetailsPage;
+import com.clipboard.pages.TelevisonsPage;
+import com.clipboard.reports.ExtentLogger;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
+
+public class ProductDetailsTests extends BaseTest {
+
+    @Test
+    public void verifyProductHeaderTest() {
+
+        ProductDetailsPage productDetailsPage = new TelevisonsPage()
+                .navigatetoTelevisionsPage()
+                .filterByBrandName()
+                .sortByPriceHighToLow()
+                .openTheItemByIndex(2)
+                .switchToProductDetailsPage();
+
+
+
+
+        Assertions.assertThat(productDetailsPage.getAboutThisItem())
+                .isEqualTo( "About this item");
+
+        ExtentLogger.info(productDetailsPage.getProductDetails().toString());
+
+    }
+
+
+}
