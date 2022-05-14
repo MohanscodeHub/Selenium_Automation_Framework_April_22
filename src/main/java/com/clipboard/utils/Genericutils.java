fd4a18da4cda0dcd1fef06c57f6cceeab2f0f385
MohanscodeHub <Krishnanmohan49@yahoo.com>
2022-05-14 22:34:18 +0530

    Added a method to sort list of items

--- src/main/java/com/clipboard/utils/Genericutils.java
+++ src/main/java/com/clipboard/utils/Genericutils.java
@@ -14,25 +14,26 @@ public final class Genericutils {
 
     }
 
-    public int selectSecondHighestitem(int index){
 
-
-        List< WebElement> items =DriverManager.getDriver().findElements(By.xpath("//span[@class='a-price-whole']"));
-        List<Integer> priceList = new ArrayList<>();
-        for(WebElement el:items){
-            priceList.add(Integer.parseInt(el.getText()));
-        }
-        Collections.sort(priceList);
-        return priceList.get(index);
-    }
-
-    public static List<String> getListOfItems(By by) {
-        List< WebElement> items =DriverManager.getDriver().findElements(by);
+    public static List<String> getListOfItemsAsString(By by) {
+        List<WebElement> items = DriverManager.getDriver().findElements(by);
         List<String> values = new ArrayList<>();
-        for(WebElement el:items){
+        for (WebElement el : items) {
             values.add(el.getText());
         }
         return values;
+
+    }
+
+
+    public static List<Integer> sortListOfItemsInDescendingOrder(List<String> values ) {
+        List<Integer> items = new ArrayList<>();
+        for (String el : values) {
+          items.add(Integer.valueOf(el.replace(",","")));
+
+        }
+        Collections.sort(items, Collections.reverseOrder());
+        return items;
 
     }
 
